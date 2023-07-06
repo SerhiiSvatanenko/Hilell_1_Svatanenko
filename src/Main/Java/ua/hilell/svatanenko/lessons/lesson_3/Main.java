705f26dd9cb8a253ab7be709c761af507af8ab31
Svatanenko Serhii <owl4973@gmail.com>
2023-07-07 00:36:40 +0300

    TeSt commiT 2

--- src/Main/Java/ua/hilell/svatanenko/lessons/lesson_3/Main.java
+++ src/Main/Java/ua/hilell/svatanenko/lessons/lesson_3/Main.java
@@ -3,5 +3,9 @@ package ua.hilell.svatanenko.lessons.lesson_3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi dears");
+        System.out.println("Hi dears");
+        System.out.println("Hi dears");
+        System.out.println("Hi dears");
+        System.out.println("Hi dears");
     }
 }
