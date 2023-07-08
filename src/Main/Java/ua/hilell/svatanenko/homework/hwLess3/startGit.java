ef51a3490c78d7efc3ebbe1a5888b5c6cc8ed894
Svatanenko Serhii <owl4973@gmail.com>
2023-07-08 22:31:38 +0300

    TeSt commiT 2

---
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess3/startGit.java
@@ -0,0 +1,9 @@
+package ua.hilell.svatanenko.homework.hwLess3;
+
+public class startGit {
+    public static void main(String[] args) {
+        System.out.println("Починаємо вивчати роботу з репозиторіями.");
+    }
+    // Починаємо вивчати роботу з репозиторіями.
+
+}
---
+++ src/Main/Java/ua/hilell/svatanenko/lessons/lesson_4/Main.java
@@ -0,0 +1,47 @@
+package ua.hilell.svatanenko.lessons.lesson_4;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println(12+5);
+//        System.out.println(12-5);
+//        System.out.println(12*5);
+//        System.out.println(12/5);
+
+//        int a = 10;
+//        int b = 3;
+
+//        System.out.println(12+5);
+//        System.out.println(12-5);
+//        System.out.println(12*5);
+//        System.out.println(12/5);
+//
+//        System.out.println((double)a/b);
+//        System.out.println(a/(double)b);
+//        System.out.println((double)a/(double)b);
+//
+//        System.out.println(10.0/3);
+//        int a = 10;
+//        int b = 5;
+//        int result = a++ + ++b - --a;
+//        System.out.println(result);
+//        System.out.println(a);
+//        System.out.println(b);
+
+        int a = 700;
+        int b = 2000;
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
