aa21a7eaf383afe17cb822b5ef3186689968414e
Svatanenko Serhii <owl4973@gmail.com>
2023-07-28 19:14:59 +0300

    №2 ДЗ 11. Транспонування. Якась халепа.. №2

--- src/Main/Java/ua/hilell/svatanenko/homework/hwLess9/Transpon.java
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess9/Transpon.java
@@ -24,7 +24,7 @@ public class Transpon {
 
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                System.out.print(Arrays.toString(array));
+                System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
