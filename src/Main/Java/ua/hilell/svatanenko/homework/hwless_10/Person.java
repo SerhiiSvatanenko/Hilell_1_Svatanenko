ae42f675b371ca1d6e33a90cc31519d4d3b52d60
Svatanenko Serhii <owl4973@gmail.com>
2023-08-01 22:56:00 +0300

    ДЗ 12. personInfo()

--- src/Main/Java/ua/hilell/svatanenko/homework/hwless_10/Person.java
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwless_10/Person.java
@@ -1,13 +1,19 @@
 package ua.hilell.svatanenko.homework.hwless_10;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Person {
     public static void main(String[] args) {
+
+        personinfo("Олександр ", "Долгих ", "Вінниця", "+830788881122");
         personinfo("Денис ", "Лонжанський ", "Київ", "+380998998777");
         personinfo("Костянтин ", "Бондаренко ", "Одеса", "+380657773338");
-        personinfo("Олександр ", "Долгих ", "Вінниця", "+830788881122");
+
     }
 
-    static void personinfo(String name, String sname, String sity, String tel) {
-        System.out.println("Зателефонувати громадянинові " + name + sname + "з міста" + sity + " можна за номером " + tel);
+    static String personinfo(String name, String sname, String sity, String tel) {
+        String txt = " Зателефонувати громадянинові " + name + sname + "з міста " + sity + " можна за номером " + tel;
+        System.out.println(txt);
+        return txt;
     }
 }
