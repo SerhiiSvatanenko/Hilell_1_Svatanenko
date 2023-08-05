24ec9d5aa1bb18ea85e88320fdb024292fa100a0
Svatanenko Serhii <owl4973@gmail.com>
2023-08-05 15:16:00 +0300

    Burgers

---
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess11/BurgerMain.java
@@ -0,0 +1,11 @@
+package ua.hilell.svatanenko.homework.hwLess11;
+
+public class BurgerMain {
+    public static void main(String[] args) {
+       Burgers first = new Burgers();
+
+       first.fullBurg();
+
+
+    }
+}
---
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess11/Burgers.java
@@ -0,0 +1,26 @@
+package ua.hilell.svatanenko.homework.hwLess11;
+
+public class Burgers {
+
+    String bun = "bun";
+    String meat = "meat";
+    String cheese = "cheese";
+    String green = "green";
+    String mayo = "mayo";
+    int wtf = 1234;
+
+    void fullBurg(){
+         System.out.println ("Склад бургеру - " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo + ", ");
+        return wtf;
+    }
+
+    int getQntt(){
+        System.out.println("Qwontity is "+wtf);
+
+    }
+
+//    doBurgers () {
+//        System.out.println("Склад бургеру - " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo + ", ");
+//    }
+}
+
