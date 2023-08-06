85fa995fb5e942bfe3bcba7bc06da8021401655d
Svatanenko Serhii <owl4973@gmail.com>
2023-08-06 20:10:57 +0300

    #2   Burgers

--- src/Main/Java/ua/hilell/svatanenko/homework/hwLess11/BurgerMain.java
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess11/BurgerMain.java
@@ -2,9 +2,8 @@ package ua.hilell.svatanenko.homework.hwLess11;
 
 public class BurgerMain {
     public static void main(String[] args) {
-       Burgers first = new Burgers();
+        Burgers first = new Burgers();
 
-       first.fullBurg();
 
 
     }
--- src/Main/Java/ua/hilell/svatanenko/homework/hwLess11/Burgers.java
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess11/Burgers.java
@@ -1,26 +1,30 @@
-package ua.hilell.svatanenko.homework.hwLess11;
 
 public class Burgers {
-
-    String bun = "bun";
-    String meat = "meat";
-    String cheese = "cheese";
-    String green = "green";
-    String mayo = "mayo";
-    int wtf = 1234;
-
-    void fullBurg(){
-         System.out.println ("Склад бургеру - " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo + ", ");
-        return wtf;
+    Burgers() {
+        String bun = "bun";
+        String meat = "meat";
+//        System.out.println("he");
     }
 
-    int getQntt(){
-        System.out.println("Qwontity is "+wtf);
 
-    }
+
+//    String bun = "bun";
+//    String meat = "meat";
+//    String cheese = "cheese";
+//    String green = "green";
+//    String mayo = "mayo";
+//    int wtf = 1234;
+//
+//    void fullBurg(){
+//         System.out.println ("Склад бургеру - " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo + ", ");
+//    }
+
+//    static String fullburg (String, String, String, String, String);
+
+
+}
 
 //    doBurgers () {
 //        System.out.println("Склад бургеру - " + bun + ", " + meat + ", " + cheese + ", " + green + ", " + mayo + ", ");
 //    }
-}
 
