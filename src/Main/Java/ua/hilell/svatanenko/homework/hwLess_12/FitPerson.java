2e19cd30fa8118e3a7c33ed736ef8d4917f087cd
Svatanenko Serhii <owl4973@gmail.com>
2023-08-13 23:15:35 +0300

    ДЗ 14. Get & Set. Some trabl #2

--- src/Main/Java/ua/hilell/svatanenko/homework/hwLess_12/FitPerson.java
+++ src/Main/Java/ua/hilell/svatanenko/homework/hwLess_12/FitPerson.java
@@ -1,22 +1,23 @@
 package ua.hilell.svatanenko.homework.hwLess_12;
 
 public class FitPerson {
-    private String fName = "Petya";
-    private String bDay = "01";
-    private String bMonth = "02";
+    private String fName;
+    private String bDay;
+    private String bMonth;
     private String bYear = "1983";
-    private String eMail = "some1mail";
-    private String phone = "some1phone";
+    private String eMail;
+    private String phone;
     public String sName;
     public String weigt;
     public String bPressure;
     public String qttStepDay;
-    public int age = 2023 - Integer.parseInt (bYear);
+    public int age = 2023 - Integer.parseInt(bYear);
 
-    public FitPerson(String fName, String sName, String bDay, String bMonth, String bYear, String eMail, String phone, String weigt, String bPressure, String qttStepDay) {
+    public FitPerson(String "Petya", String sName, String "01", String "02", String "1983", String "some@email", String "+380671112244", String weigt, String bPressure, String qttStepDay) {
 
     }
-        void printAccountInfo (){
-        System.out.println(fName + " "+sName+ " "+ age + " "+bPressure+" "+ weigt+ " "+qttStepDay+ " " + phone + " " + eMail);
+
+    void printAccountInfo() {
+        System.out.println(fName + " " + sName + " " + age + " " + bPressure + " " + weigt + " " + qttStepDay + " " + phone + " " + eMail);
     }
 }
